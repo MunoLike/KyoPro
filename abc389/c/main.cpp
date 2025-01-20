a037d718b5d347aee4da071846b97a3f9a71dbc5
MunoLike <sukorice@gmail.com>
2025-01-20 05:17:31 +0000

    ABC389分を追加

---
+++ abc389/c/main.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using i32 = int32_t;
+using i64 = int64_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using mint = modint998244353;
+#define rep(i, n) for (i32 i = 0; i < (n); i++)
+#define rep1(i, n) for (i32 i = 1; i <= (n); (i)++)
+#define all(a) (a).begin(), (a).end()
+const i32 dy[] = {-1, 0, 1, 0, -1, -1, 1, 1};
+const i32 dx[] = {0, 1, 0, -1, -1, 1, 1, -1};
+const string lrud("URDL");
+
+template <class S, class T>
+inline S prevent_oor(S a, const T b) {
+    a %= b;
+    if (a < 0) {
+        a += b;
+    }
+    return a;
+}
+
+template <class S, class T>
+inline bool is_in(const T lower, S a, const T upper) {
+    return a < upper && lower <= a;
+}
+
+template <class S, class T>
+inline S chmax(S &a, T b) {
+    if (a < b) {
+        a = b;
+    }
+    return a;
+}
+
+template <class S, class T>
+inline S chmin(S &a, T b) {
+    if (a > b) {
+        a = b;
+    }
+    return a;
+}
+
+inline int64_t div_floor(int64_t a, int64_t b) {
+    if (b < 0) {
+        a *= -1;
+        b *= -1;
+    }
+    if (a < 0) {
+        return (a + 1) / b - 1;
+    } else {
+        return a / b;
+    }
+}
+
+inline int64_t div_ceil(int64_t a, int64_t b) {
+    return div_floor(a + b - 1, b);
+}
+
+int main() {
+    i32 q;
+    cin >> q;
+
+    deque<pair<i64, i64>> snakes;  // head, len
+
+    i64 decrement = 0, tail = 0;
+    rep(i, q) {
+        i32 query;
+        cin >> query;
+        switch (query) {
+            case 1:
+                i64 l;
+                cin >> l;
+                snakes.emplace_back(tail, l);
+                tail += l;
+                break;
+            case 2:
+                decrement += snakes.front().second;
+                snakes.pop_front();
+                break;
+            case 3:
+                i64 k;
+                cin >> k;
+
+                cout << snakes[k - 1].first - decrement << '\n';
+                break;
+            default:
+                break;
+        }
+    }
+}
