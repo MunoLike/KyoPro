17817a62821a3abdb1ab6717009c35e0f3cac2ce
MunoLike <sukorice@gmail.com>
2025-02-13 17:44:43 +0000

    ABC333 Dを追加

--- abc333/d/main.cpp
+++ abc333/d/main.cpp
@@ -2,48 +2,64 @@
 
 #include <atcoder/all>
 using namespace std;
+using namespace atcoder;
 using i32 = int32_t;
 using i64 = int64_t;
 using u32 = uint32_t;
 using u64 = uint64_t;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
-#define all(a) (a).begin(),(a).end()
+#define all(a) (a).begin(), (a).end()
 const i32 dy[] = {-1, 0, 1, 0, -1, -1, 1, 1};
 const i32 dx[] = {0, 1, 0, -1, -1, 1, 1, -1};
 
 template <class S, class T>
 inline S prevent_oor(S a, const T b) {
-  a %= b;
-  if (a < 0) {
-    a += b;
-  }
-  return a;
+    a %= b;
+    if (a < 0) {
+        a += b;
+    }
+    return a;
 }
 
 template <class S, class T>
 inline S chmax(S &a, T b) {
-  if (a < b) {
-    a = b;
-  }
-  return a;
+    if (a < b) {
+        a = b;
+    }
+    return a;
 }
 
 inline int64_t div_floor(int64_t a, int64_t b) {
-  if (b < 0) {
-    a *= -1;
-    b *= -1;
-  }
-  if (a < 0) {
-    return (a + 1) / b - 1;
-  } else {
-    return a / b;
-  }
+    if (b < 0) {
+        a *= -1;
+        b *= -1;
+    }
+    if (a < 0) {
+        return (a + 1) / b - 1;
+    } else {
+        return a / b;
+    }
 }
 
 inline int64_t div_ceil(int64_t a, int64_t b) {
-  return div_floor(a+b-1, b);
+    return div_floor(a + b - 1, b);
 }
 
 int main() {
+    i32 n;
+    cin >> n;
+    dsu uf(n);
+    rep(i, n - 1) {
+        i32 u, v;
+        cin >> u >> v;
+        --u, --v;
+        
+        if (u != 0) {
+            uf.merge(u, v);
+        }
+    }
 
+    auto gs = uf.groups();
+    auto it = (*max_element(all(gs), [](auto e1, auto e2) { return e1.size() < e2.size(); })).size();
+    cout << n - it << endl;
 }
