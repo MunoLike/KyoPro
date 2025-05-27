e7321de4d6691343b87c28ee7d0b8857d47ced9c
MunoLike <sukorice@gmail.com>
2025-05-27 13:18:28 +0000

    ABC407 A~Cを解いた。

--- tmp/abc.cpp
+++ tmp/abc.cpp
@@ -1,6 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
+template <class S, class T>
+inline S prevent_oor(S a, const T b) {
+    a %= b;
+    if (a < 0) {
+        a += b;
+    }
+    return a;
+}
 int main() {
     int a = 10;
-    cout << setprecision(10) << a / (double)3 << endl;
+    cout << prevent_oor(-7, 10) << endl;
 }
